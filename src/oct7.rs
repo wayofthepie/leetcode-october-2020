03cbaac55275ab6c98ea99bbde7e08915fb54fbc
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-10-07 18:52:02 +0100

    Clean up solution for oct7

--- src/oct7.rs
+++ src/oct7.rs
@@ -17,43 +17,46 @@ impl ListNode {
 struct Solution;
 
 impl Solution {
-    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        head.as_ref()?;
+    pub fn rotate_right(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
         if k == 0 {
             return head;
         }
-        let length = {
-            let mut last = head.as_mut().unwrap();
-            let mut length = 1;
-            while last.next.is_some() {
-                length += 1;
-                last = last.next.as_mut().unwrap();
-            }
-            length
-        };
-        let mut moves = length - (k % length) - 1;
-        let mut node = head.as_mut().unwrap();
-        let mut new_head = Box::new(ListNode::new(node.val));
-        let mut build_pointer = &mut new_head;
-        while moves > 0 {
+        let mut head = head?;
+        let length = Solution::get_length(&mut head);
+        let moves = length - (k % length) - 1;
+        let mut node = head.as_mut();
+        let mut builder = Box::new(ListNode::new(node.val));
+        let mut build_pointer = &mut builder;
+        for _ in 0..moves {
             node = node.next.as_mut().unwrap();
-            let new = Box::new(ListNode::new(node.as_ref().val));
-            build_pointer.next = Some(new);
+            build_pointer.next = Some(Box::new(ListNode::new(node.val)));
             build_pointer = build_pointer.next.as_mut().unwrap();
-            moves -= 1;
         }
-        if node.next.is_some() {
-            node = node.next.as_mut().unwrap();
-            let mut real_head = node.clone();
-            let mut build_pointer = &mut real_head;
-            while build_pointer.next.is_some() {
-                build_pointer = build_pointer.next.as_mut().unwrap();
-            }
-            build_pointer.next = Some(new_head);
-            Some(real_head)
+        if let Some(mut next) = node.next.as_mut() {
+            Solution::build_final_list(&mut next, builder)
         } else {
-            head
+            Some(head)
         }
+    }
+
+    fn build_final_list(node: &mut Box<ListNode>, builder: Box<ListNode>) -> Option<Box<ListNode>> {
+        let mut head = node.clone();
+        let mut build_pointer = &mut head;
+        while build_pointer.next.is_some() {
+            build_pointer = build_pointer.next.as_mut().unwrap();
+        }
+        build_pointer.next = Some(builder);
+        Some(head)
+    }
+
+    fn get_length(head: &mut Box<ListNode>) -> i32 {
+        let mut last = head.as_mut();
+        let mut length = 1;
+        while last.next.is_some() {
+            length += 1;
+            last = last.next.as_mut().unwrap();
+        }
+        length
     }
 }
 
